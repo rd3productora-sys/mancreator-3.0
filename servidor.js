84274fda0003f1adba08d6bf078dbcb2104020f6
rd3productora-sys <rd3productora@gmail.com>
2026-09-21 21:43:23 -0300

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
